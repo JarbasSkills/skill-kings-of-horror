1634c2252d5d9a3d0cdceb8f7cafcd2c63ff3d26
jarbasal <jarbasai@mailfence.com>
2021-08-03 18:22:38 +0100

    bump ovos version

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from mycroft.skills.core import intent_file_handler
 from pyvod import Collection, Media
 from os.path import join, dirname, basename
-from ovos_workshop.frameworks.cps import CPSMatchType, CPSPlayback, \
+from ovos_workshop.frameworks.playback import CPSMatchType, CPSPlayback, \
     CPSMatchConfidence
 from ovos_workshop.skills.video_collection import VideoCollectionSkill
 
