1381db28b18adcf17f86c978134b1dde258b7f41
jarbasal <jarbasai@mailfence.com>
2021-08-14 14:02:14 +0100

    .

--- __init__.py
+++ __init__.py
@@ -1,17 +1,17 @@
 from mycroft.skills.core import intent_file_handler
 from pyvod import Collection, Media
 from os.path import join, dirname, basename
-from ovos_workshop.frameworks.playback import CPSMatchType, CPSPlayback, \
-    CPSMatchConfidence
+from ovos_workshop.frameworks.playback import CommonPlayMediaType, CommonPlayPlaybackType, \
+    CommonPlayMatchConfidence
 from ovos_workshop.skills.video_collection import VideoCollectionSkill
 
 
 class KingsofHorrorSkill(VideoCollectionSkill):
     def __init__(self):
         super().__init__("KingsofHorror")
-        self.supported_media = [CPSMatchType.GENERIC,
-                                CPSMatchType.MOVIE,
-                                CPSMatchType.VIDEO]
+        self.supported_media = [CommonPlayMediaType.GENERIC,
+                                CommonPlayMediaType.MOVIE,
+                                CommonPlayMediaType.VIDEO]
         self.message_namespace = basename(dirname(__file__)) + ".jarbasskills"
         # load video catalog
         path = join(dirname(__file__), "res", "KingsofHorror.jsondb")
@@ -22,8 +22,8 @@ class KingsofHorrorSkill(VideoCollectionSkill):
         self.default_image = join(dirname(__file__), "ui", "logo.png")
         self.skill_icon = join(dirname(__file__), "ui", "logo.png")
         self.default_bg = join(dirname(__file__), "ui", "bg.jpg")
-        self.playback_type = CPSPlayback.GUI
-        self.media_type = CPSMatchType.MOVIE
+        self.playback_type = CommonPlayPlaybackType.GUI
+        self.media_type = CommonPlayMediaType.MOVIE
 
     # voice interaction
     def get_intro_message(self):
@@ -52,7 +52,7 @@ class KingsofHorrorSkill(VideoCollectionSkill):
     def match_media_type(self, phrase, media_type):
         score = 0
 
-        if self.voc_match(phrase, "video") or media_type == CPSMatchType.VIDEO:
+        if self.voc_match(phrase, "video") or media_type == CommonPlayMediaType.VIDEO:
             score += 5
 
         if self.voc_match(phrase, "horror"):
@@ -63,7 +63,7 @@ class KingsofHorrorSkill(VideoCollectionSkill):
         if self.voc_match(phrase, "indie"):
             score += 10
 
-        if self.voc_match(phrase, "movie") or media_type == CPSMatchType.MOVIE:
+        if self.voc_match(phrase, "movie") or media_type == CommonPlayMediaType.MOVIE:
             score += 30
 
         if self.voc_match(phrase, "kings-of-horror"):
